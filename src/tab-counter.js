3edba384b8fa00e8f4b5c0f3b9a9c634bf83c982
Shubanker Chourasia <subhankerchourasia@gmail.com>
2018-12-28 14:17:29 +0530

    Initial Commit

---
+++ src/tab-counter.js
@@ -0,0 +1,84 @@
+function tabCount(onTabCountUpdate,preventInitialFire){
+    /**
+     * @updateInterval: interval in milli seconds to count/update active tabs status.
+     * minimum value: 1000
+     */
+    var updateInterval = 2000;
+    /**
+     * @TabId: unique id for this tab.
+     */
+    var tabId = Math.random().toString(36).substring(7);
+    preventInitialFire = preventInitialFire===undefined?false:preventInitialFire;
+    var self = this;
+    var tabsCount = 0;
+    var updateActive = function(){
+        var data = getData();
+        data.list[tabId]=Date.now();
+        updateData(data);
+        if(self.onTabCountUpdate!==undefined || tabsCount===0){
+            self.tabsCount(false);
+        }
+    }
+    self.tabsCount = function(skipCallback){
+        skipCallback = skipCallback===undefined?true:skipCallback;
+        var data=getData();
+        var listIds=Object.keys(data.list);
+        var now=Date.now();
+        var count = 0;
+        listIds.forEach(id => {
+            if(data.list[id]+updateInterval*1.2>now){
+                count++;
+            }
+        });
+        if(!skipCallback && tabsCount !== count){
+            if(self.onTabCountUpdate !==undefined){
+                onTabCountUpdate(count);
+            }       
+        }
+        return tabsCount = count;
+    }
+    /**
+     * Cleans data of closed tabs
+     */
+    var clearList = function(){
+        var data=getData();
+        var listIds=Object.keys(data.list);
+        var now=Date.now();
+        listIds.forEach(id => {
+            if(data.list[id]+8000<now){//If tab last update is 8 seconds or older get rid of it.
+                delete data.list[id];
+            }
+        });
+        updateData(data);
+    }
+    var updateData = function(data){
+        localStorage.setItem('tabCountData',typeof(data)==="string"?data:JSON.stringify(data));
+    }
+    var getData = function(){
+        savedData = localStorage.getItem('tabCountData');
+        return savedData== null ?{list:{}}:JSON.parse(savedData);
+    }
+    var updateActiveInterval = setInterval(() => {
+        updateActive();
+    }, updateInterval);
+    var clearListInterval = setInterval(() => {
+        clearList();
+    }, 20000);
+    
+    /**
+     * Initialise 
+     */
+    updateActive();
+    this.onTabCountUpdate = onTabCountUpdate;
+    if(!preventInitialFire){
+        this.onTabCountUpdate(tabsCount);
+    }
+    window.onbeforeunload=function(e){
+        var data = getData();
+        delete data.list[tabId];
+        updateData(data);
+    }
+    return {
+        tabsCount:self.tabsCount
+    };
+}
